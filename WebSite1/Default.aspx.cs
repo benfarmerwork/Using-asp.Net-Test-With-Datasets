d24ab50ae67a1db84c39798721a896f6be5e1616
Ben Farmer <ben_farmer3@hotmail.com>
2016-09-01 19:01:16 +0100

    Add files via upload

--- WebSite1/Default.aspx.cs
+++ WebSite1/Default.aspx.cs
@@ -9,7 +9,7 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        
     }
 
     protected void Button1_Click(object sender, EventArgs e)
@@ -25,5 +25,26 @@ public partial class _Default : System.Web.UI.Page
             txtCourseName.Text = (string)courseTableAdapter.GetCourseDescription(courseId);
         else
             txtCourseName.Text = "Invalid CourseId Entered";
+    }
+
+    protected void btnOpenQuery_Click(object sender, EventArgs e)
+    {
+        var studentsTableAdapter = new StudentsDSTableAdapters.StudentsTableAdapter();
+
+        var studentDataTable = studentsTableAdapter.GetDataByStudentSecondName(txtSurname.Text);
+        
+        DataGrid1.DataSourceID = null;
+        DataGrid1.DataSource = studentDataTable;
+        DataGrid1.DataBind();
+    }
+
+    protected void SearchAge_Click(object sender, EventArgs e)
+    {
+        // An Adapter is the section that sits between the databse and the c# code
+        var studentsTableAdapter = new StudentsDSTableAdapters.StudentsTableAdapter();
+        var studentDataTable = studentsTableAdapter.GetDataByAge(int.Parse(txtSearchAge.Text));
+        DataGrid1.DataSourceID = null;
+        DataGrid1.DataSource = studentDataTable;
+        DataGrid1.DataBind();
     }
 }
